169b2c2763c9ff7eeee1f89724066414daa461e1
MuriithiGichuru <muriithigichuru@gmail.com>
2019-09-11 09:33:55 +0300

    Python Basics

---
+++ Hello World.py
@@ -0,0 +1,5 @@
+# The line below prints Hello world to the console. NB, putting a hashtag infront of a line makes it a comment and pycharm overlooks it.
+# control foward slash is another way of commenting out the line
+print("Hello World")
+print('hello world')
+#we are now moving to python varialbles
---
+++ variables.py
@@ -0,0 +1,11 @@
+#A variable is a representation of a memory location. They are stored in the RAM and not in the hard disk
+# print(45+67+78+45+56)
+# print((45+67+78+45+56)/5)
+# #if you do not put quotes, it will calculate.
+# howards_marks = 45+67+78+45+56+67+78+67
+# print((howards_marks)/8)
+
+#Data types -  they can be represented in
+#numbers - integers, floats, doubles
+#Boolean -  True or false
+#Strings -
